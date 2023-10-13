bc2b6c196e32d85083f7f4edd1bb47d7bc9e3ba1
Achmad Rifa'i <janoko.java22@yahoo.com>
2023-10-13 18:56:13 +0700

    loader

--- fe/src/componenents/Loader.jsx
+++ fe/src/componenents/Loader.jsx
@@ -1,10 +1,9 @@
 import * as React from 'react'
 import { Backdrop, CircularProgress } from '@mui/material'
-import { useSelector, useDispatch } from 'react-redux'
-import { loaded } from '../redux/loader'
+import { useSelector } from 'react-redux'
 
 export default function Loader() {
-    const load = useSelector(state => state.loader.load), dispatch = useDispatch()
+    const load = useSelector(state => state.loader.load)
     return <Backdrop open={load} sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <CircularProgress size='30em' color='inherit' />
     </Backdrop>
